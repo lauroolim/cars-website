772043daa070474d087c6391bccb712204862837
lauroolim <lauroolimota743@gmail.com>
2024-10-16 18:04:50 -0300

    CRUD cars

--- veiculo/urls.py
+++ veiculo/urls.py
@@ -1,8 +1,11 @@
 # sistema/urls.py
 from django.urls import path
-from veiculo.views import ListarVeiculos, FotoVeiculo
+from .views import ListarVeiculos, CriarVeiculos, EditarVeiculos, ExcluirVeiculos, FotoVeiculo
 
 urlpatterns = [
     path('listar/', ListarVeiculos.as_view(), name='listar-veiculos'),
     path('upload-foto/', FotoVeiculo, name='upload-foto'),
+    path('editar/<int:pk>/', EditarVeiculos.as_view(), name='editar-veiculos'),
+    path('criar/', CriarVeiculos.as_view(), name='criar-veiculos'),
+    path('excluir/<int:pk>/', ExcluirVeiculos.as_view(), name='excluir-veiculos'),
 ]
